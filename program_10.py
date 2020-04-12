2e15b6d08b3cd22878a32ed284c57515ff448040
Linji Wang <wang1278@scholar-fe04.rcac.purdue.edu>
2020-04-12 18:07:07 -0400

    Assignment10

--- program_10.py
+++ program_10.py
@@ -1,4 +1,6 @@
 #!/bin/env python
+# Modified on April 10, 2020
+#  by Linji Wang
 # Created on March 25, 2020
 #  by Keith Cherkauer
 #
@@ -10,7 +12,6 @@
 #
 import pandas as pd
 import scipy.stats as stats
-import numpy as np
 
 def ReadData( fileName ):
     """This function takes a filename as input, and returns a dataframe with
@@ -32,7 +33,8 @@ def ReadData( fileName ):
                          delimiter=r"\s+",parse_dates=[2], comment='#',
                          na_values=['Eqp'])
     DataDF = DataDF.set_index('Date')
-    
+    # remove negative values
+    DataDF=DataDF[~(DataDF['Discharge']<0)]
     # quantify the number of missing values
     MissingValues = DataDF["Discharge"].isna().sum()
     
@@ -42,7 +44,10 @@ def ClipData( DataDF, startDate, endDate ):
     """This function clips the given time series dataframe to a given range 
     of dates. Function returns the clipped dataframe and and the number of 
     missing values."""
-    
+    # clip the dataframe with the given date range
+    DataDF=DataDF[startDate:endDate]
+    # quantify the number of missing values
+    MissingValues = DataDF["Discharge"].isna().sum()
     return( DataDF, MissingValues )
 
 def CalcTqmean(Qvalues):
@@ -52,7 +57,12 @@ def CalcTqmean(Qvalues):
        exceeds mean streamflow for each year. Tqmean is based on the
        duration rather than the volume of streamflow. The routine returns
        the Tqmean value for the given data array."""
-    
+    # drop nodata values
+    Qvalues = Qvalues.dropna()
+    # calculate total time
+    T=len(Qvalues)
+    # calculate Tqmean
+    Tqmean=(Qvalues>Qvalues.mean()).sum()/T
     return ( Tqmean )
 
 def CalcRBindex(Qvalues):
@@ -63,7 +73,14 @@ def CalcRBindex(Qvalues):
        values of day-to-day changes in daily discharge volumes
        (pathlength) by total discharge volumes for each year. The
        routine returns the RBindex value for the given data array."""
-    
+    # drop nodata values
+    Qvalues = Qvalues.dropna()
+    # calculate sum of absolute of day-to-day streamflow difference
+    sum_abs = 0.0    
+    for i in range(len(Qvalues)-1):
+        sum_abs += abs(Qvalues[i+1]-Qvalues[i])
+    # calculate R-B Index
+    RBindex = (sum_abs/Qvalues.sum())
     return ( RBindex )
 
 def Calc7Q(Qvalues):
@@ -74,7 +91,11 @@ def Calc7Q(Qvalues):
        picking the lowest average flow in any 7-day period during
        that year.  The routine returns the 7Q (7-day low flow) value
        for the given data array."""
-    
+    # remove nodata values
+    Qvalues = Qvalues.dropna()
+
+    # find lowest average flow in any 7-day period
+    val7Q = Qvalues.rolling(window=7).mean().min()
     return ( val7Q )
 
 def CalcExceed3TimesMedian(Qvalues):
@@ -84,7 +105,10 @@ def CalcExceed3TimesMedian(Qvalues):
        provided) and then counting the number of days with flow greater than 
        3 times that value.   The routine returns the count of events greater 
        than 3 times the median annual flow value for the given data array."""
-    
+    # remove nodata values
+    Qvalues = Qvalues.dropna()
+    # computes the number of days with flows greater than 3x annual median flow
+    median3x=(Qvalues>3*Qvalues.median()).sum()
     return ( median3x )
 
 def GetAnnualStatistics(DataDF):
@@ -92,28 +116,64 @@ def GetAnnualStatistics(DataDF):
     the given streamflow time series.  Values are retuned as a dataframe of
     annual values for each water year.  Water year, as defined by the USGS,
     starts on October 1."""
-    
+    # column names
+    colNames=['site_no','Mean Flow','Peak Flow','Median','Coeff Var','Skew','TQmean','R-B Index','7Q','3xMedian']
+    # year range
+    WYrange = range(1970,2020)
+    # define a new dataframe to store annual metric values
+    WYDataDF = pd.DataFrame(0.0, index=WYrange, columns=colNames)
+    # define the index as Water Year
+    WYDataDF.index.name = 'Water Year'
+    # calculate the metrics for each water year   
+    WYDataDF['site_no'] = DataDF['site_no'][0]
+    for WY in WYrange:
+        WYDataDF.loc[WY,'Mean Flow'] = DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"].mean()
+        WYDataDF.loc[WY,'Peak Flow'] = DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"].max()
+        WYDataDF.loc[WY,'Median'] = DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"].median()
+        WYDataDF.loc[WY,'Coeff Var'] = DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"].std()/WYDataDF.loc[WY,'Mean Flow']*100
+        WYDataDF.loc[WY,'Skew'] = stats.skew(DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"])
+        WYDataDF.loc[WY,'TQmean'] = CalcTqmean(DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"])
+        WYDataDF.loc[WY,'R-B Index'] = CalcRBindex(DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"])
+        WYDataDF.loc[WY,'7Q'] = Calc7Q(DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"])
+        WYDataDF.loc[WY,'3xMedian'] = CalcExceed3TimesMedian(DataDF['Discharge'][str(WY-1)+"-10-01":str(WY)+"-09-30"])        
+    WYDataDF.index = DataDF.resample('AS-OCT').mean().index
     return ( WYDataDF )
 
 def GetMonthlyStatistics(DataDF):
     """This function calculates monthly descriptive statistics and metrics 
     for the given streamflow time series.  Values are returned as a dataframe
     of monthly values for each year."""
-
+    # column names
+    colNames=['site_no','Mean Flow','Coeff Var','TQmean','R-B Index']
+    # resample
+    index = DataDF.resample('MS').mean().index
+    # define a new dataframe to store monthly metric values
+    MoDataDF = pd.DataFrame(0.0, index=index, columns=colNames)
+    # define the index as Month
+    MoDataDF.index.name = 'Month'
+    # calculate the metrics for each month
+    MoDataDF['site_no'] = DataDF['site_no'][0]
+    MoDataDF['Mean Flow'] = DataDF['Discharge'].resample("MS").mean().values
+    MoDataDF['Coeff Var'] = DataDF['Discharge'].resample("MS").std().values/MoDataDF['Mean Flow']*100
+    MoDataDF['TQmean'] = DataDF['Discharge'].resample("MS").apply(CalcTqmean).values
+    MoDataDF['R-B Index'] = DataDF['Discharge'].resample("MS").apply(CalcRBindex).values
     return ( MoDataDF )
 
 def GetAnnualAverages(WYDataDF):
     """This function calculates annual average values for all statistics and
     metrics.  The routine returns an array of mean values for each metric
     in the original dataframe."""
-    
+    AnnualAverages=WYDataDF.mean(axis=0)
     return( AnnualAverages )
 
 def GetMonthlyAverages(MoDataDF):
     """This function calculates annual average monthly values for all 
     statistics and metrics.  The routine returns an array of mean values 
     for each metric in the original dataframe."""
-    
+    # calculate mean for each month by grouping
+    Mo_index=lambda x:x.month
+    MonthlyAverages = MoDataDF.groupby(Mo_index).mean()
+    MonthlyAverages.index.name = 'Date'
     return( MonthlyAverages )
 
 # the following condition checks whether we are running as a script, in which 
@@ -164,4 +224,19 @@ if __name__ == '__main__':
         MonthlyAverages[file] = GetMonthlyAverages(MoDataDF[file])
         
         print("-"*50, "\n\nSummary of monthly metrics...\n\n", MoDataDF[file].describe(), "\n\nAnnual Monthly Averages...\n\n", MonthlyAverages[file])
+
+        # add a column for river names as Station and output annual to .csv
+        WYDataDF[file].insert(0,'Station',file)
+        WYDataDF[file].to_csv('Annual_Metrics.csv', mode='a')
+        # add a column for river names as Station and output annual to .txt
+        AA_df = pd.DataFrame(AnnualAverages[file])
+        Station = pd.DataFrame(file,index=['Station'],columns=[0])
+        AA_Df = Station.append(AA_df)
+        AA_Df.to_csv('Average_Annual_Metrics.txt', sep='\t', mode='a', header=False)
         
+        # add a column for river names as Station and output monthly to .csv
+        MoDataDF[file].insert(0,'Station',file)
+        MoDataDF[file].to_csv('Monthly_Metrics.csv', mode='a')
+        # add a column for river names as Station and output monthly to .txt
+        MonthlyAverages[file].insert(0,'Station',file)
+        MonthlyAverages[file].to_csv('Average_Monthly_Metrics.txt', sep='\t', mode='a', header=True)
